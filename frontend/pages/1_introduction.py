355303d0becfab09c7145edad55f5bd60c2d1bea
Mohnish Shah <shahmohnish@SAPSE.customer.alter.net>
2024-02-04 11:52:09 -0800

    intro statement

--- frontend/pages/1_introduction.py
+++ frontend/pages/1_introduction.py
@@ -1,10 +1,30 @@
 import streamlit as st
 
 def introduction_page():
-    st.title("Meet the Team - Introduction")
+        # Introduction
+    st.title("LlamaLogic: Order from Chaos")
+    
+    st.markdown(
+        """
+        In today's data-centric environment, the need for well-organized, structured data representation is 
+        pervasive across various individuals and organizations. Summarizing extensive data sources in a specified 
+        structured manner presents a significant challenge. Traditional Large Language Models (LLMs) often 
+        face difficulties in maintaining uniformity when organizing unstructured data consistently. This 
+        inconsistency becomes a barrier to achieving a standardized and precise interpretation of information. 
+        Recognizing the widespread demand for consistently structured data representation and the challenges 
+        associated with data summarization, our solution introduces a groundbreaking approach.
+
+        By incorporating Retrieval-Augmented Generation (RAG) technology, amplified by LlamaIndex, our project 
+        provides an efficient and reliable solution to the challenges posed by unstructured data. It not only 
+        ensures the consistent structuring of extensive information but also adeptly navigates the limitations 
+        imposed by token constraints. This unique capability empowers our solution to deliver a precise, 
+        structured interpretation of data consistently, even when dealing with massive datasets!
+        """
+    )
+    
 
     st.write("""
-        Welcome to our application! Let's get to know the amazing team behind the scenes.
+        Before we dive into our demo, let's first get to know the amazing team behind the scenes.
     """)
 
     team_members = [
